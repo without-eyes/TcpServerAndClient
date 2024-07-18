e777cf00a14953e970ad7dfae0f46fb994fafef4
without-eyes <dimka180205@gmail.com>
2024-07-18 18:00:45 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main() {
+    // create socket
+    int network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // specify an address for the socket
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    int connection_status = connect(network_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+    // check for the error with the connection
+    if (connection_status == -1) {
+        puts("There was ana error making a connection to the remote socket!\n\n");
+    }
+
+    return 0;
+}
